2e3dac486fed2ad9b9aae1eba431ffcf210f59a6
stewri707 <41470850+stewri707@users.noreply.github.com>
2024-09-29 20:37:11 +0200

    Create bloodhoundpy.sh

---
+++ setup_user_scripts/bloodhoundpy.sh
@@ -0,0 +1,11 @@
+echo ==================== Bloodhoud.py Bloodhound CE Branch
+echo "Bloodhoud.py Bloodhound CE Branch. Press Enter"
+read blaj
+cd $HOME/venv
+git clone -b bloodhound-ce https://github.com/dirkjanm/BloodHound.py.git
+mv BloodHound.py bloodhound.py
+python3 -m venv bloodhound.py
+.  bloodhound.py/bin/activate
+cd bloodhound.py/
+pip install .
+deactivate
