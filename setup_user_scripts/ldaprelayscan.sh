14ed81e48a5593e3bbc580a1236024cff14fc740
stewri707 <41470850+stewri707@users.noreply.github.com>
2024-10-05 16:00:45 +0200

    Create ldaprelayscan.sh

---
+++ setup_user_scripts/ldaprelayscan.sh
@@ -0,0 +1,11 @@
+echo ================ LdapRelayScan
+echo "LdapRelayScan, Press Enter to continue..."
+read blaj
+cd $HOME/venv
+git clone https://github.com/zyn3rgy/LdapRelayScan.git
+mv LdapRelayScan ldaprelayscan
+python3 -m venv ldaprelayscan
+. ldaprelayscan/bin/activate
+cd ldaprelayscan
+pip install -r requirements_exact.txt
+deactivate
