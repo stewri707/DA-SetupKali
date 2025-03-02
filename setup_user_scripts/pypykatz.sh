6c6d162db62a7b5ba7e322d1cfa4dadd3d838c1d
stewri707 <41470850+stewri707@users.noreply.github.com>
2025-03-02 13:21:20 +0100

    Create pypykatz.sh

---
+++ setup_user_scripts/pypykatz.sh
@@ -0,0 +1,10 @@
+echo ============ PypyKatz
+echo "PypyKatz, Press Enter to continue..."
+read blaj
+cd $HOME/venv
+git clone https://github.com/skelsec/pypykatz.git
+python3 -m venv pypykatz
+. pypykatz/bin/activate
+cd pypykatz/
+pip3 install minidump minikerberos aiowinreg msldap winacl
+deactivate
