599891efc71906f3f9d2d054a377bf1f89f1ca1c
Steven Wright <stewri707@gmail.com>
2025-08-21 14:10:58 +0200

    Add script to download Visual Studio Code extensions

---
+++ PS/GetVsCodeExtensions.ps1
@@ -0,0 +1,31 @@
+param(
+    $VsixDir = 'C:\Util\VSCodeExtension'
+)
+
+# Info on an extension; https://marketplace.visualstudio.com/items?itemName=<ExtensionId>
+# Note: Publisher must not contain Space.
+$Extensions = @(
+    @{Publisher='Microsoft'; Id = 'ms-vscode.PowerShell'},
+    @{Publisher='yzhang'; Id = 'yzhang.markdown-all-in-on'}
+)
+
+if (Test-Path $VsixDir) {
+    if (-not (Get-Item $VsixDir).PSIsContainer) {
+        throw "$VsixDir exists and is a file."
+    }
+}
+else
+{
+    try {
+        New-Item -ItemType Directory -Path $VsixDir -ErrorAction Stop | Out-Null
+    } catch {
+        throw "Failed to create directory: $VsixDir"
+    }
+}
+
+foreach ( $Extension in $Extensions)
+{
+    $URI = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/$($Extension.Publisher)/vsextensions/$($Extension.Name)/latest/vspackage"
+    $Destination = Join-Path -Path $VsixDir -ChildPath "$($Extension.Id).vsix"
+    curl.exe -L -o $Destination $URI
+}
