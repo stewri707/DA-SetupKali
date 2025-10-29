a8c0b9198f27d083e86aef5743c17d5b5b896edb
stewri707 <41470850+stewri707@users.noreply.github.com>
2025-10-29 08:23:23 +0100

    Add script to automate GPOddity setup
    This script automates the setup of the GPOddity project by cloning the repository, creating a virtual environment, and installing dependencies.

---
+++ setup_user_scripts/gpoddity.sh
@@ -0,0 +1,10 @@
+echo ==================== GPOddity.py
+echo "GPOddity.py. Press Enter"
+read blaj
+cd $HOME/venv
+git clone https://github.com/synacktiv/GPOddity
+python3 -m venv GPOddity
+.  GPOddity/bin/activate
+cd GPOddity/
+pip install -r requirements.txt
+deactivate
