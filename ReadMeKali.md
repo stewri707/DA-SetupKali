4393f4334606dceac613c18f034a9a836e4ea1bd
Steven Wright <stewri707@gmail.com>
2026-06-04 11:06:04 +0200

    venv dir

--- ReadMeKali.md
+++ ReadMeKali.md
@@ -2,12 +2,18 @@
 ```
 # As root
 sudo su -
+apt update
+cd ~localuser/DA-SetupKali
+git fetch
+git pull
+
 cd ~localuser/DA-SetupKali/bash
 find . -type f -name "*.sh" -exec chmod ug+x {} \;
 ./setup_root.sh
 
 # No longer as root
 logout
+
 cd ~localuser/DA-SetupKali/bash
 ./setup_user.sh
 
--- bash/setup_user_scripts/venv_dir.sh
+++ bash/setup_user_scripts/venv_dir.sh
@@ -3,9 +3,22 @@
 # =========== Katalog för venv ===========
 echo "Make dir for PIPENV, Press Enter to continue..."
 read blaj
+
 if [ -d "$HOME/venv" ]; then
-	echo "Directory already exists: $HOME/venv"
+    echo "Directory already exists: $HOME/venv"
+    
+    # Prompt the user for confirmation
+    read -p "Do you want to delete all its contents? (y/Y to confirm): " reply
+    
+    # Check if the user input matches 'y' or 'Y'
+    if [ "$reply" = "y" ] || [ "$reply" = "Y" ]; then
+        # Deletes all files and subdirectories inside, including hidden files
+        find "$HOME/venv" -mindepth 1 -delete
+        echo "All contents of $HOME/venv have been deleted."
+    else
+        echo "Operation canceled. Contents preserved."
+    fi
 else
-	mkdir "$HOME/venv"
-	echo "Directory created: $HOME/venv"
+    mkdir "$HOME/venv"
+    echo "Directory created: $HOME/venv"
 fi
